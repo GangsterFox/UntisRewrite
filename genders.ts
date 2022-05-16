02581ca50b505a5d582a015371d6496778391be8
Fox <diniscool06@gmail.com>
2022-05-16 14:56:15 +0200

    changed a few things

---
+++ genders.ts
@@ -0,0 +1,20 @@
+const WebUntisLib = require('webuntis');
+
+const QRCodeData = 'untis://setschool?url=herakles.webuntis.com&school=GoldenB Schule&user=HayvanDin&key=AJCO5JHYCHBIFEUC&schoolNumber=5401500';
+const untis = new WebUntisLib.WebUntisQR(QRCodeData);
+
+untis
+    .login()
+    .then(() => {
+        return untis.getStudents();
+    })
+    .then((fam) => {
+        let females = [];
+        let males = [];
+        for (const user of fam) {
+            if (user.gender == "female") females.push(user);
+            else if (user.gender == "male") males.push(user)
+        }
+        console.log('females: ' + females.length);
+        console.log('males: ' + males.length)
+    }), untis.logout()
