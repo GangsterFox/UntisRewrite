5c06af5170b8539b07439e2c1f87933f702bad8a
Fox <diniscool06@gmail.com>
2022-05-17 12:13:13 +0200

    write to a json

--- genders.ts
+++ genders.ts
@@ -1,10 +1,20 @@
 const WebUntisLib = require('webuntis');
-
+const fs = require('fs');
 const QRCodeData = 'untis://setschool?url=herakles.webuntis.com&school=GoldenB Schule&user=HayvanDin&key=AJCO5JHYCHBIFEUC&schoolNumber=5401500';
 const untis = new WebUntisLib.WebUntisQR(QRCodeData);
 
+// this is the worst way to make it write to a json and also console log it fr
+// I will make this not a pain someday but not today because this shit works 
+
 untis
     .login()
+    .then(() => {
+        return untis.getStudents();
+    })
+    .then((students) => {
+        fs.writeFileSync('./students.json', JSON.stringify(students))
+        console.log('written all students to a json')
+    })
     .then(() => {
         return untis.getStudents();
     })
@@ -18,4 +28,4 @@ untis
         console.log('females:', females.length);
         console.log('males:',  males.length);
         console.log('Everyone:', males.length + females.length)
-    }), untis.logout()
+    })
