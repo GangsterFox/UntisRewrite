4128d06ad69596ae6408f76d9272ca3e8fc709d5
Fox <diniscool06@gmail.com>
2022-05-17 12:16:38 +0200

    write to a json better

--- genders.ts
+++ genders.ts
@@ -3,24 +3,16 @@ const fs = require('fs');
 const QRCodeData = 'untis://setschool?url=herakles.webuntis.com&school=GoldenB Schule&user=HayvanDin&key=AJCO5JHYCHBIFEUC&schoolNumber=5401500';
 const untis = new WebUntisLib.WebUntisQR(QRCodeData);
 
-// this is the worst way to make it write to a json and also console log it fr
-// I will make this not a pain someday but not today because this shit works 
-
 untis
     .login()
-    .then(() => {
-        return untis.getStudents();
-    })
-    .then((students) => {
-        fs.writeFileSync('./students.json', JSON.stringify(students))
-        console.log('written all students to a json')
-    })
     .then(() => {
         return untis.getStudents();
     })
     .then((fam) => {
         let females = [];
         let males = [];
+        fs.writeFileSync('./testing.json', JSON.stringify(fam))
+        console.log('written all students to a json')
         for (const user of fam) {
             if (user.gender == "female") females.push(user);
             else if (user.gender == "male") males.push(user)
