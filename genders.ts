6876e72894297095da371339fbcb2806708b8510
Fox <diniscool06@gmail.com>
2022-05-23 13:20:56 +0200

    add uknown gender

--- genders.ts
+++ genders.ts
@@ -14,15 +14,18 @@ untis
     .then((fam) => {
         let females = [];
         let males = [];
+        let unknown = [];
         fs.writeFileSync('./students.json', JSON.stringify(fam))
         console.log('written all students to a json \n')
         for (const user of fam) {
             if (user.gender == "female") females.push(user);
-            else if (user.gender == "male") males.push(user)
+            else if (user.gender == "male") males.push(user);
+            else if (user.gender == "") unknown.push(user)
         }
         console.log('females:', females.length);
         console.log('males:',  males.length);
-        console.log('Everyone:', males.length + females.length);
+        console.log('unknown:', unknown.length);
+        console.log('Everyone:', males.length + females.length + unknown.length);
         untis.logout()
         console.log('\nlogged out')
     })
