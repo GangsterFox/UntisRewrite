3235a0777af4d7fbc4cab549fdceccd8cf0d054a
Fox <diniscool06@gmail.com>
2022-05-24 23:23:35 +0200

    add teachers and rename to students

--- students.ts
+++ students.ts
@@ -11,13 +11,13 @@ untis
     .then(() => {
         return untis.getStudents();
     })
-    .then((fam) => {
+    .then((students) => {
         let females = [];
         let males = [];
         let unknown = [];
-        fs.writeFileSync('./students.json', JSON.stringify(fam))
+        fs.writeFileSync('./students.json', JSON.stringify(students))
         console.log('written all students to a json \n')
-        for (const user of fam) {
+        for (const user of students) {
             try {
             if (user.gender == "female") females.push(user);
             else if (user.gender == "male") males.push(user);
@@ -28,6 +28,22 @@ untis
         }
         console.log('females:', females.length, '\nmales:', males.length,
         '\nunknown:', unknown.length, '\nEveryone:', males.length + females.length + unknown.length);
+    })
+    .then(() => {
+        return untis.getTeachers();
+    })
+    .then((teachers) => {
+        let teacher = [];
+        fs.writeFileSync('./teachers.json', JSON.stringify(teachers))
+        console.log("\nwritten all teachers to a json")
+        for (const teacher1 of teachers) {
+            try {
+                if (teacher1.title == "") teacher.push(teacher1)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+        console.log("teachers:", teacher.length)
         untis.logout()
-        console.log('\nlogged out')
+        console.log("\nlogged out")
     })
--- test.ts
+++ test.ts
@@ -1,6 +1,5 @@
 const WebUntisLib = require('webuntis');
 const fs = require('fs');
-const tomorrow = new Date()
 const dotenv = require('dotenv')
 dotenv.config()
 const QRCodeData = process.env.QRCodeData
@@ -10,12 +9,19 @@ const untis = new WebUntisLib.WebUntisQR(QRCodeData);
 untis
     .login()
     .then(() => {
-        return untis.getOwnClassTimetableFor(tomorrow);
+        return untis.getTeachers();
     })
     .then((answer) => {
+        let teacher = [];
         fs.writeFileSync('./test.json', JSON.stringify(answer))
-        console.log("success")
+        for (const user of answer) {
+            try {
+                if (user.title == "") teacher.push(user)
+            } catch (error) {
+                console.log(error)
+            }; 
+        }
+        console.log("teachers:", teacher.length)
+        untis.logout();
+        console.log('logged out');
     })
-    untis.logout()
-    .then(() =>
-    console.log('logged out'));
