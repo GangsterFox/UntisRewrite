40649d71ed2f2745fdb62773b69dd008ca06376f
Fox <diniscool06@gmail.com>
2022-05-30 12:03:02 +0200

    clean up more

--- test.ts
+++ test.ts
@@ -6,7 +6,6 @@ const QRCodeData = process.env.QRCodeData
 const untis = new WebUntisLib.WebUntisQR(QRCodeData);
 
 untis.login().then(async () => {
-
 let females = [];
 let males = [];
 let unknown = [];
@@ -16,30 +15,30 @@ const students = await untis.getStudents();
 const teachers = await untis.getTeachers();
 
 fs.writeFileSync('./students.json', JSON.stringify(students))
-console.log('written all students to a json \n')
+fs.writeFileSync('./teachers.json', JSON.stringify(teachers))
+console.log("written all students and teachers to seperate json's\n")
 const entrys = require("./students.json")
-    for (const user of entrys) {
-        try {
-            if (user.gender == "female") females.push(user);
-            else if (user.gender == "male") males.push(user);
-            else if (user.gender == "") unknown.push(user)
-            } catch (error) {
-                console.log(error)
-            };
+const entrys1 = require("./teachers.json")
+
+for (const user of entrys) {
+    try {
+        if (user.gender == "female") females.push(user)
+        else if (user.gender == "male") males.push(user)
+        else if (user.gender == "") unknown.push(user)
+        } catch (error) {
+            console.log(error)
         }
+}
 console.log('females:', females.length, '\nmales:', males.length, '\nunknown:', unknown.length, '\nEveryone:', males.length + females.length + unknown.length);
 
-fs.writeFileSync('./teachers.json', JSON.stringify(teachers))
-console.log("\nwritten all teachers to a json")
-const entrys1 = require("./teachers.json")
 for (const teacher1 of entrys1) {
     try {
         if (teacher1.title == "") teacher.push(teacher1)
         } catch (error) {
             console.log(error)
-        }
-    }
-console.log("teachers:", teacher.length)
+        };
+}
+console.log("\nteachers:", teacher.length)
 
 }).then(() => {
     untis.logout()
