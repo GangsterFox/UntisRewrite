d21788be9d2fbe7613f82730be8f40a3fe6ec2a6
Fox <diniscool06@gmail.com>
2022-05-30 10:19:09 +0200

    clean it up a bit

--- test.ts
+++ test.ts
@@ -3,7 +3,6 @@ const fs = require('fs');
 const dotenv = require('dotenv');
 dotenv.config()
 const QRCodeData = process.env.QRCodeData
-
 const untis = new WebUntisLib.WebUntisQR(QRCodeData);
 
 untis.login().then(async () => {
@@ -16,12 +15,11 @@ let teacher = [];
 const students = await untis.getStudents();
 const teachers = await untis.getTeachers();
 
-if (students) {
-    fs.writeFileSync('./students.json', JSON.stringify(students))
-    console.log('written all students to a json \n')
-    const entrys = require("./students.json")
-        for (const user of entrys) {
-            try {
+fs.writeFileSync('./students.json', JSON.stringify(students))
+console.log('written all students to a json \n')
+const entrys = require("./students.json")
+    for (const user of entrys) {
+        try {
             if (user.gender == "female") females.push(user);
             else if (user.gender == "male") males.push(user);
             else if (user.gender == "") unknown.push(user)
@@ -29,21 +27,21 @@ if (students) {
                 console.log(error)
             };
         }
-        console.log('females:', females.length, '\nmales:', males.length,
-        '\nunknown:', unknown.length, '\nEveryone:', males.length + females.length + unknown.length);
-}
-if (teachers) {
-    fs.writeFileSync('./teachers.json', JSON.stringify(teachers))
-    console.log("\nwritten all teachers to a json")
-    const entrys = require("./teachers.json")
-    for (const teacher1 of entrys) {
-        try {
-            if (teacher1.title == "") teacher.push(teacher1)
+console.log('females:', females.length, '\nmales:', males.length, '\nunknown:', unknown.length, '\nEveryone:', males.length + females.length + unknown.length);
+
+fs.writeFileSync('./teachers.json', JSON.stringify(teachers))
+console.log("\nwritten all teachers to a json")
+const entrys1 = require("./teachers.json")
+for (const teacher1 of entrys1) {
+    try {
+        if (teacher1.title == "") teacher.push(teacher1)
         } catch (error) {
             console.log(error)
         }
     }
-    console.log("teachers:", teacher.length)
-}
-}).then(() => {    untis.logout()
-    console.log("\nlogged out")})
+console.log("teachers:", teacher.length)
+
+}).then(() => {
+    untis.logout()
+    console.log('\nlogged out')
+});
