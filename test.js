4aa126d69cb1fe8d4c0337e227dab995d1218087
Lily Fox <lilyfox.dev@gmail.com>
2024-09-18 09:39:26 +0200

    Pretty print

--- test.js
+++ test.js
@@ -1,10 +1,12 @@
 const WebUntis = require("webuntis");
 const fs = require("fs");
-const untis = new WebUntis.WebUntisAnonymousAuth('GWS Villingen', 'kephiso.webuntis.com');
+require("dotenv").config();
+//const untis = new WebUntis.WebUntisAnonymousAuth('GWS Villingen', 'kephiso.webuntis.com');
+const untis = new WebUntis.WebUntisQR(process.env.loginDetails);
 
 untis.login().then(async () => {
 
-fs.writeFileSync('./testing.json', JSON.stringify(await untis.getClasses()));
+fs.writeFileSync('./output/testing.json', JSON.stringify(await untis.getStatusData(), null, 2));
 const testing = JSON.parse(fs.readFileSync('./output/testing.json'));
 
 console.log(`Amount: ${testing.length}`);
